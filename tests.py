bc5063e62f06b9180a06317583afa2d7439cbed3
Roman Popov <Jun1or14@yandex.kz>
2025-04-15 11:56:52 +0500

    My first commit

--- tests.py
+++ tests.py
@@ -1,24 +1,103 @@
-from main import BooksCollector
+import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    #добавление несколько книг без жанра, используя параметризацию
+    @pytest.mark.parametrize('name',
+                             ['Анжелика', 'Путь Абая']
+                             )
+    def test_add_new_books_in_dict_added(self, books_collector, name):
+        books_collector.add_new_book(name)
+        expected_dict = books_collector.books_genre.keys()
+        assert name in expected_dict
+    #добавление несколько книг с жанром
+    @pytest.mark.parametrize('name, genre',
+                             [
+                                 ['Терминатор', 'Фантастика'],
+                                 ['Микки маус', 'Мультфильмы']
+                             ])
+    def test_set_book_genre_in_dict_added(self, books_collector, name, genre):
+        books_collector.add_new_book(name)
+        books_collector.set_book_genre(name, genre)
+        assert genre in books_collector.books_genre.values()
+    #Проверка жанра
+    def test_get_book_genre_dict_is_received(self, books_collector):
+        books_collector.add_new_book('Микки маус')
+        books_collector.set_book_genre('Микки маус', 'Мультфильмы')
+        assert books_collector.get_book_genre('Микки маус') == 'Мультфильмы'
+    #Добавление книги без жанра
+    def test_get_book_genre_no_genre_added(self, books_collector):
+        books_collector.add_new_book('Терминатор')
+        assert books_collector.get_book_genre('Терминатор') == ''
+    #Список с жанром книг
+    @pytest.mark.parametrize('name, genre',
+                             [
+                                 ['Омен', 'Ужасы'],
+                                 ['Стражы галактики', 'Фантастика']
+                             ])
+    def test_get_books_with_specific_genre_books_received(self, books_collector, name, genre):
+        books_collector.add_new_book(name)
+        books_collector.set_book_genre(name, genre)
+        assert books_collector.get_books_with_specific_genre(genre) == [name]
+    # проверка словаря books_genre
+    @pytest.mark.parametrize('name, genre',
+                             [
+                                 ['Укол зонтиком', 'Комедии'],
+                                 ['Русалочка', 'Мультфильмы'],
+                                 ['Дэдпулл', 'Фантастика'],
+                                 ['38 попугаев', 'Мультфильмы'],
+                                 ['Крик', 'Ужасы'],
+                                 ['Странные игры', 'Детективы']
+                             ])
+    def test_get_books_genre_books_genre_received(self, books_collector, name, genre):
+        books_collector.add_new_book(name)
+        books_collector.set_book_genre(name, genre)
+        assert books_collector.get_books_genre() == {name: genre}
+    #проверка книг детям
+    @pytest.mark.parametrize('name, genre',
+                             [
+                                 ['Маска', 'Комедии'],
+                                 ['Виннипух', 'Мультфильмы'],
+                                 ['Машина времени', 'Фантастика']
+                             ])
+    def test_get_books_for_children_book_is_received(self, books_collector, name, genre):
+        books_collector.add_new_book(name)
+        books_collector.set_book_genre(name, genre)
+        assert books_collector.get_books_for_children() == [name]
+    #проверка книг детям "неподходящих"
+    @pytest.mark.parametrize('name, genre',
+                             [
+                                 ['Дракула', 'Ужасы'],
+                                 ['Концы в воду', 'Детектив']
+                             ])
+    def test_get_books_for_children_book_is_not_received(self, books_collector, name, genre):
+        books_collector.add_new_book(name)
+        books_collector.set_book_genre(name, genre)
+        assert books_collector.get_books_for_children() != [name]
+    #проверка добавление в избранное
+    def test_add_book_in_favorites_in_list_is_added(self, books_collector):
+        name = 'Анжелика'
+        books_collector.add_new_book(name)
+        books_collector.add_book_in_favorites(name)
+        favorite_books = books_collector.get_list_of_favorites_books()
+        assert name in favorite_books
+    #проверка удаления
+    def test_delete_book_from_favorites_book_is_deleted(self, books_collector):
+        name = 'Анжелика'
+        books_collector.add_new_book(name)
+        books_collector.add_book_in_favorites(name)
+        books_collector.delete_book_from_favorites(name)
+        assert name not in books_collector.favorites
+    #проверка избранного
+    @pytest.mark.parametrize('name, genre',
+                             [
+                                 ['Шерлок','Детектив'],
+                                 ['Алиса','Фантастика']
+                             ])
+    def test_get_list_of_favorites_books_have_books(self, books_collector, genre, name):
+        books_collector.add_new_book(name)
+        books_collector.set_book_genre(name, genre)
+        books_collector.add_book_in_favorites(name)
+        assert name in books_collector.get_list_of_favorites_books()
